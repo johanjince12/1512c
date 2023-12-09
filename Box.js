72425e386a3881beb357c840ff563fa0f22dfb43
johanjince12 <96847741+johanjince12@users.noreply.github.com>
2023-12-09 10:53:45 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+// Registering component in box-component.js
+AFRAME.registerComponent("move-box-on-click", {
+  schema: {
+    moveX: { type: "number", default: 1 },
+  },
+
+  tick: function () {
+
+    this.data.moveX = this.data.moveX + 0.01;
+
+    var pos = this.el.getAttribute("position");
+
+    pos.x = this.data.moveX;
+
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+    var pos = this.el.getAttribute("position");
+    pos.x = pos.x + this.data.moveX;
+    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+  }
+  
+});
